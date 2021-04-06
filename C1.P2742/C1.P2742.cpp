2de1f97b639668d1cc3e6ee0145eeed1afc986ee
hskim <tiryul@gmail.com>
2021-04-06 21:07:04 +0900

    #Add new project : C1.P2742  - Solved.

---
+++ C1.P2742/C1.P2742.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+#define scanf scanf_s
+
+int main()
+{
+	int n;
+	if (!scanf("%d", &n))
+	{
+		return 0;
+	};
+	if (n < 1 || n > 100000)
+	{
+		return 0;
+	}
+	while (n)
+	{
+		printf("%d\n", n);
+		n--;
+	}
+	return 0;
+}
