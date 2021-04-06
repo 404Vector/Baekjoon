16fd34b46c44728a8d5cb51fd82239040b6dd58a
hskim <tiryul@gmail.com>
2021-04-06 21:15:00 +0900

    #Add new project : C1.P2753  - Solved.

---
+++ C1.P2753/C1.P2753.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int year;
+	cin >> year;
+
+	bool b4 = !(year % 4);
+	bool b100 = !(year % 100);
+	bool b400 = !(year % 400);
+	if ((b4 && !b100) || (b400))
+	{
+		cout << 1 << "\n";
+	}
+	else
+	{
+		cout << 0 << "\n";
+	}
+}
