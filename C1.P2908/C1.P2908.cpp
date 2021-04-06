6c6a6134d376f561294f7b4f54edfb2de4b94c3b
hskim <tiryul@gmail.com>
2021-04-06 21:45:02 +0900

    #Add new project : C1.P2908  - Solved.

---
+++ C1.P2908/C1.P2908.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+static int Reverse(int n)
+{
+	int result = 0;
+	while (n > 0)
+	{
+		result *= 10;
+		result += (n % 10);
+		n /= 10;
+	}
+	return result;
+}
+
+int main()
+{
+	int n1, n2;
+	scanf_s("%d %d", &n1, &n2);
+	n1 = Reverse(n1);
+	n2 = Reverse(n2);
+	if (n1 > n2)
+	{
+		printf("%d\n", n1);
+	}
+	else
+	{
+		printf("%d\n", n2);
+	}
+}
