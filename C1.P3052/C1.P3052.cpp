d401457dadfb2a7f9af0a67187aa1eb29598b8eb
hskim <tiryul@gmail.com>
2021-04-06 22:25:51 +0900

    #Add new project : C1.P3052  - Solved.

---
+++ C1.P3052/C1.P3052.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+const int keyValue = 42;
+
+int main()
+{
+	int inputArray[10] = { 0, };
+	int modStackArray[10] = { 0, };
+	int count = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		int v;
+		bool isOld = false;
+		cin >> v;
+		if (v > 1000 || v < 0) { return 0; }
+		inputArray[i] = v;
+		modStackArray[i] = v % keyValue;
+		for (int j = i - 1; j >= 0; j--)
+		{
+			isOld = isOld || (modStackArray[i] == modStackArray[j]);
+		}
+		count = (isOld) ? count : count + 1;
+	}
+	std::cout << count << endl;
+
+	return 0;
+}
