9a8ad57813b6bdb830d44fa55c3679ffa7c62125
hskim <tiryul@gmail.com>
2021-04-06 20:35:56 +0900

    #Add new project : C1.P2739  - Solved.

---
+++ C1.P2739/C1.P2739.cpp
@@ -0,0 +1,15 @@
+﻿// C1.P2739.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i < 10; i++)
+	{
+		cout << n << " * " << i << " = " << n * i << endl;
+	}
+}
