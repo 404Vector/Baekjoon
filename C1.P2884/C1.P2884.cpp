25189b69f4d632ed2a001ae34f697405e5019fbe
hskim <tiryul@gmail.com>
2021-04-06 21:34:24 +0900

    #Add new project : C1.P2884  - Solved.

---
+++ C1.P2884/C1.P2884.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+const int mSift = 45;
+
+int main()
+{
+	int hour, minute;
+	int dhour, dminute;
+	scanf_s("%d %d", &hour, &minute);
+	if (hour < 0 || hour > 23) { return -1; }
+	if (minute < 0 || minute > 59) { return -1; }
+	minute -= mSift;
+	dminute = (minute + 60) % 60;
+	dhour = (minute > -1) ? (minute / 60) : -1;
+	dhour = (hour + dhour + 24) % 24;
+	printf("%d %d", dhour, dminute);
+	return 0;
+}
