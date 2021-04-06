a51b9846d43ab07299bb346e5097458799441660
hskim <tiryul@gmail.com>
2021-04-06 22:43:30 +0900

    #Add new project : C1.P9498  - Solved.

---
+++ C1.P9498/C1.P9498.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	int score;
+	std::cin >> score;
+	char grad = (score >= 90) ? 'A'
+		: (score >= 80) ? 'B'
+		: (score >= 70) ? 'C'
+		: (score >= 60) ? 'D'
+		: 'F';
+	printf("%c", grad);
+}
