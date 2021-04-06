df615beb0a7b24fda6385b998973397df66e3373
hskim <tiryul@gmail.com>
2021-04-06 23:42:10 +0900

    #Add new project : C1.P10809  - Solved.

---
+++ C1.P10809/C1.P10809.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	char S[101] = { 0, };
+	char A[27] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 0 };
+
+	scanf_s("%s", S, sizeof(S));
+
+	for (int i = 0; i < 26; i++) // 알파벳 개수만큼 반복
+	{
+		int C = 0;
+		for (int j = 0; j < sizeof(S); j++)
+		{
+			// baekjoon .... b : a, b, c...
+			if (A[i] == S[j])
+			{
+				C = j;
+				break;
+			}
+		}
+		if (A[i] == S[C])
+		{
+			printf("%d ", C);
+		}
+		else
+		{
+			printf("-1 ");
+		}
+	}
+	return 0;
+}
