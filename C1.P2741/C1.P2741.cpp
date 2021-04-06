45143ab526cb8b5d5f73214cc4506dff2a80009e
hskim <tiryul@gmail.com>
2021-04-06 21:02:07 +0900

    #Add new project : C1.P2741  - Solved.

---
+++ C1.P2741/C1.P2741.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	if (!scanf("%d", &n))
+	{
+		return 0;
+	};
+	if (n < 1 || n > 100000)
+	{
+		return 0;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d\n", i + 1);
+	}
+	return 0;
+}
