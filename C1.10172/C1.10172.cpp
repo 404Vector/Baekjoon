7c46413fd1878b89fabd74f529f0c403572a7860
hskim <tiryul@gmail.com>
2021-04-06 22:54:52 +0900

    #Add new project : C1.P10172  - Solved.

---
+++ C1.10172/C1.10172.cpp
@@ -0,0 +1,13 @@
+﻿// C1.10172.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|\n");
+}
