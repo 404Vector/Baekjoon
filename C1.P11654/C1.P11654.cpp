f6a7357ed1f01a89cc34a49c5b6af79fb8119a94
404Vector <tiryul@gmail.com>
2021-04-08 01:51:29 +0900

    #Add new project : C1.P10998, C1.P11654, C1.P11720  - Solved.

---
+++ C1.P11654/C1.P11654.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	char A;
+	cin >> A;
+	cout << (int)A << endl;
+}
---
+++ C1.P11720/C1.P11720.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+main()
+{
+	int a, c, i;
+	char b;
+	c = 0;
+	cin >> a;
+	for (i = 0; i < a; i++)
+	{
+		cin >> b;
+		c += b - 48;
+	}
+	cout << c << endl;
+}
