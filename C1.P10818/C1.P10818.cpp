fd688b3833e4cafe67bf391c2638514f5841915b
404Vector <tiryul@gmail.com>
2021-04-08 00:56:36 +0900

    #Add new project : C1.P10818  - Solved.

---
+++ C1.P10818/C1.P10818.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+
+int main()
+{
+    int num;
+
+    scanf_s("%d", &num);
+
+    int* arr = new int[num];
+
+    int max = -1000000;
+
+    int min = 1000000; 
+
+    for (int i = 0; i < num; i++)
+    {
+        std::cin >> arr[i];
+        min = (arr[i] < min) ? arr[i] : min;
+        max = (arr[i] > max) ? arr[i] : max;
+    }
+    std::cout << min << " " << max;
+    delete arr;
+    return 0;
+}
