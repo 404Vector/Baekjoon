c251280a8bcb9d81ad7934787c42fe22e22e51e7
hskim <tiryul@gmail.com>
2021-04-08 21:56:23 +0900

    #Add new project : C2.P1259  - Solved.

---
+++ C2.P1259/C2.P1259.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+static int Reverse(int n)
+{
+	int result = 0;
+	while (n > 0)
+	{
+		result *= 10;
+		result += (n % 10);
+		n /= 10;
+	}
+	return result;
+}
+
+int main()
+{
+	while (true)
+	{
+		int value, rValue;
+		cin >> value;
+		rValue = Reverse(value);
+		if (value == 0) {
+			break;
+		}
+		if (value == rValue) {
+			cout << "yes" << "\n";
+		}
+		else
+		{
+			cout << "no" << "\n";
+		}
+	}
+	return 0;
+}
