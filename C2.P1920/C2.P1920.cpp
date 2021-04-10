67c19b8cd397c922610f98f71c47cf7d1c1402c5
hskim <tiryul@gmail.com>
2021-04-10 20:32:28 +0900

    #Create Project : C2.P1920  - Solved.

---
+++ C2.P1920/C2.P1920.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define int64 long long
+using namespace std;
+
+static int IsIncluded(int64& value, vector<int64>& collection, int cursorLeft, int cursorRight)
+{
+	while (cursorLeft <= cursorRight)
+	{
+		int cursorMid = (cursorLeft + cursorRight) / 2;
+		if (collection[cursorMid] == value)
+		{
+			return 1;
+		}
+		else if (collection[cursorMid] < value)
+		{
+			cursorLeft = cursorMid + 1;
+		}
+		else
+		{
+			cursorRight = cursorMid - 1;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	int numberOfA, numberOfB;
+	vector<int64> A, B;
+
+	cin >> numberOfA;
+	for (int i = 0; i < numberOfA; i++)
+	{
+		int64 value;
+		cin >> value;
+		A.push_back(value);
+	}
+
+	cin >> numberOfB;
+	for (int i = 0; i < numberOfB; i++)
+	{
+		int64 value;
+		cin >> value;
+		B.push_back(value);
+	}
+
+	sort(A.begin(), A.end());
+
+	for (int i = 0; i < numberOfB; i++)
+	{
+		int val = IsIncluded(B[i], A, 0, numberOfA - 1);
+		printf("%d\n", val);
+	}
+}
