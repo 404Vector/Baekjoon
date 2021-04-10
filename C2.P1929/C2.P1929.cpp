4aed7f8b3977660c09df7a42937d73688f9674f2
hskim <tiryul@gmail.com>
2021-04-10 21:26:16 +0900

    #Create Project : C2.P1929  - Soved.

---
+++ C2.P1929/C2.P1929.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static vector<bool> GetPrimeArray(int rangeMax)
+{
+	vector<bool> PrimeArray(rangeMax + 1, true);
+	PrimeArray[0] = false;
+	PrimeArray[1] = false;
+	for (int i = 2; i * i <= rangeMax; i++)
+	{
+		if (PrimeArray[i])
+		{
+			for (int j = i * i; j <= rangeMax; j += i)
+			{
+				PrimeArray[j] = false;
+			}
+		}
+	}
+	return PrimeArray;
+}
+
+int main()
+{
+	int minRange, maxRange;
+	cin >> minRange >> maxRange;
+
+	auto primeArray = GetPrimeArray(maxRange);
+	for (int i = minRange; i <= maxRange; i++)
+	{
+		if (primeArray[i])
+		{
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
+}
